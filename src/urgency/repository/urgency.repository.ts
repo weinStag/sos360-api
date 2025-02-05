26bfd117bbef1665c879e207d9ac1a1b1041cff9
Renan Kohatsu <rkohatsu@unifesp.br>
2025-02-05 19:26:00 -0300

    new ffeat: Urgency (emegia rápida)

---
+++ src/urgency/repository/urgency.repository.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/service/prisma.service';
+import { Prisma } from '@prisma/client';
+import { urgencyInput } from '../input/urgency.input';
+
+@Injectable()
+export class Urgencyrepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  public async findAllurgencies() {
+    return this.prismaService.urgency.findMany();
+  }
+
+  public async findUrgencyById(id: string) {
+    return this.prismaService.urgency.findUnique({ where: { id } });
+  }
+
+  public async addUrgency(urgency: urgencyInput) {
+    const data: Prisma.urgenceCreateInput = {
+      type: urgency.type,
+      description: urgency.description,
+      status: urgency.status }
+    }
+
+    return this.prismaService.urgency.create({ data });
+  }
+
+  public async removeUrgencyById(id: string) {
+    return this.prismaService.urgency.delete({ where: { id } });
+  }
+}
---
+++ src/urgency/urgency.resolver.ts
@@ -0,0 +1,81 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+
+import { Query } from '@nestjs/graphql';
+import { CustomLogger } from 'src/logger/custom.logger';
+import { Status } from '@prisma/client';
+import { Urgencyrepository } from './repository/urgency.repository';
+import { urgencySchema } from './schema/urgency.schema';
+
+@Resolver()
+export class UrgencyResolver {
+  constructor(
+    private UrgencyRepository: Urgencyrepository,
+    private logger: CustomLogger,
+  ) { }
+
+  @Query(() => [urgencySchema])
+  async urgencies() {
+    this.logger.log('Received request to find all urgencies')
+    try {
+      const urgencies = await this.UrgencyRepository.findAllurgencies();
+      this.logger.log(`Fetched ${urgencies.length} urgencies`);
+
+      return urgencies;
+    } catch (error) {
+      this.logger.error('Error fetching all urgencies');
+      throw error;
+    }
+  }
+
+  @Query(() => [urgencySchema])
+  async findUrgenciesByStatus(@Args(`status`) status: Status) {
+    try {
+      const allUrgencies = await this.UrgencyRepository.findAllurgencies();
+      return allUrgencies.filter((urgency) => urgency.status == status);
+    }catch(error) {
+      return error;
+    }
+  }
+
+  @Query(() => urgencySchema)
+  async findUrgencyById(@Args('id') id: string) {
+    this.logger.log('Received request to find a urgency by Id')
+    try {
+      const urgency = await this.UrgencyRepository.findUrgencyById(id);
+      this.logger.log(`Urgency with id ${urgency.id}`);
+
+      return urgency;
+    } catch (error) {
+      this.logger.error('Error fetching urgency by id');
+      throw error;
+    }
+  }
+
+  @Mutation(() => urgencySchema)
+  async createEmergency(@Args('urgency') urgency: urgencySchema) {
+    this.logger.log('Received request to create a new urgency');
+    try {
+      const newUrgency = await this.UrgencyRepository.addUrgency(urgency);
+      this.logger.log('Urgency created.');
+
+      return newUrgency;
+    } catch (error) {
+      this.logger.error('Error creating a new urgency');
+      throw error;
+    }
+  }
+
+  @Mutation(() => urgencySchema)
+  async removeEmergencyById(@Args('id') id: string) {
+    this.logger.log('Received request to remove a urgency by id');
+    try {
+      const urgency = await this.UrgencyRepository.removeUrgencyById(id);
+      this.logger.log(`Urgency with id ${urgency.id} removed sucessfully.`);
+
+      return urgency;
+    } catch (error) {
+      this.logger.error('Error removing urgency by id');
+      throw error;
+    }
+  }
+}
